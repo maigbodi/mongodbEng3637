ebbab6d0fe860f9b8ecae7b50e62cc1ee7ee97e8
maigbodi <34092907+maigbodi@users.noreply.github.com>
2019-08-15 15:25:32 +0100

    new files

---
+++ spec/unit/recipes/default_spec.rb
@@ -0,0 +1,32 @@
+#
+# Cookbook:: mongo
+# Spec:: default
+#
+# Copyright:: 2019, The Authors, All Rights Reserved.
+
+require 'spec_helper'
+
+describe 'mongo::default' do
+  context 'When all attributes are default, on Ubuntu 18.04' do
+    # for a complete list of available platforms and versions see:
+    # https://github.com/chefspec/fauxhai/blob/master/PLATFORMS.md
+    platform 'ubuntu', '18.04'
+
+    it 'should update source list' do
+      expect(chef_run).to update_apt_update 'update_sources'
+    end
+
+    it 'should add mongod to source list' do
+      expect(chef_run).to add_apt_repository('mongodb-org')
+    end
+
+    it 'should install mongodb' do
+      expect(chef_run).to install_package 'mongodb-org'
+    end
+
+
+    it 'converges successfully' do
+      expect { chef_run }.to_not raise_error
+    end
+  end
+end
